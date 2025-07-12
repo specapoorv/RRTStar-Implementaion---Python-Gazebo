e0b75f0765e7bb8a96b56fed456b06621646fc6d
Ananth-Pai19 <paiananth0838@gmail.com>
2025-07-13 00:38:41 +0530

    Update rrt_star_final.py (Cleaned it up, pls check)

--- rrt_star_final.py
+++ rrt_star_final.py
@@ -4,78 +4,95 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-class node:
+class Node:
     def __init__(self, position):
         self.x = position[0]
         self.y = position[1]
-        self.parent = None
+        self.parent: None | Node = None
         self.cost = 0
+
+
+    def distance(self, other) -> int:
+        if isinstance(other, Node):
+            return int(math.dist(
+                (self.x, self.y),
+                (other.x, other.y)
+            ))
+        else:
+            raise NotImplemented
+        
+    
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.x == other.x and self.y == other.y
+        else:
+            return NotImplemented
         
 
-class rrt():
+class RRT:
     def __init__(self, start_pos, goal_pos, map_size, step_size, search_radius, goal_radius, occupancy_grid):
-        self.start_node = node(start_pos)
-        self.goal_node = node(goal_pos)
+        self.start_node = Node(start_pos)
+        self.goal_node = Node(goal_pos)
         self.map_size = map_size
         self.step_size = step_size
         self.search_radius = search_radius
         self.goal_radius = goal_radius
         self.occupancy_grid = occupancy_grid
+        self.max_iterations = 3000
 
         self.debug = False
         self.goal_reached = False
         self.node_tree = [self.start_node]
 
-    def distance(self, n1, n2):
-        return (n2.x - n1.x, n2.y - n1.y) #returns dx, dy
-    
 
     def sample_point(self):
         if self.debug:
             time.sleep(0.25)
             print("sampling point")
         if random.random() < 0.05:  # 5% goal bias
-            return node((self.goal_node.x, self.goal_node.y))
+            return Node((self.goal_node.x, self.goal_node.y))
         width = self.map_size[0]
         height = self.map_size[1]
         rand_x = random.randint(0, width)
         rand_y = random.randint(0, height)
         rand_node_pos = (rand_x, rand_y)
-        rand_node = node(rand_node_pos)
-        # print(rand_node.x)
-        
+        rand_node = Node(rand_node_pos)
         return rand_node
+    
 
-    def find_nearest_node(self, node):
+    def find_nearest_node(self, node: Node):
         if self.debug:
             time.sleep(0.25)
             print("finding nearest node")
         nearest = self.node_tree[0]
-        dx, dy = self.distance(nearest, node)
-        min_dist = math.sqrt(dx**2 + dy**2)
+        # dx, dy = self.distance(nearest, node)
+        min_dist = node.distance(nearest)# math.sqrt(dx**2 + dy**2)
 
         for n in self.node_tree[1:]:
-            dx, dy = self.distance(n, node)
-            euclidean_distance = math.sqrt(dx**2 + dy**2)
+            # dx, dy = self.distance(n, node)
+            euclidean_distance = node.distance(n)# math.sqrt(dx**2 + dy**2)
 
             if euclidean_distance < min_dist:
                 nearest = n
                 min_dist = euclidean_distance
         # print(nearest)
         return nearest
+    
 
-    def brancher(self, from_node, to_node):
+    def brancher(self, from_node: Node, to_node: Node):
         if self.debug:
             time.sleep(0.25)
             print("branching")
 
-        dx, dy = self.distance(from_node, to_node)
-        theta = math.atan2(dy, dx)
+        # dx, dy = self.distance(from_node, to_node)
+        theta = math.atan2(
+            to_node.y - from_node.y, to_node.x - from_node.x
+        )
 
         new_x = int(from_node.x + self.step_size * math.cos(theta))
         new_y = int(from_node.y + self.step_size * math.sin(theta))
 
-        return node((new_x, new_y))  # No parent or cost yet (done in develop_tree)
+        return Node((new_x, new_y))  # No parent or cost yet (done in develop_tree)
     
 
     def bresenham_line(self, x0, y0, x1, y1):
@@ -110,18 +127,19 @@ class rrt():
 
         if self.debug == True:
             time.sleep(0.5)
-            print(f"[debug] returned cells fro  bresenham line = {cells}")
+            print(f"[debug] returned cells from bresenham line = {cells}")
         return cells
     
     
     def find_near_nodes(self, new_node):
-        near_nodes = []
-        for n in self.node_tree:
-            dx, dy = self.distance(n, new_node)
-            dist = math.sqrt(dx**2 + dy**2)
-            if dist <= self.search_radius:
-                near_nodes.append(n)
-        return near_nodes
+        # near_nodes = []
+        # for n in self.node_tree:
+        #     # dx, dy = self.distance(n, new_node)
+        #     dist = self.distance(n, new_node)# math.sqrt(dx**2 + dy**2)
+        #     if dist <= self.search_radius:
+        #         near_nodes.append(n)
+        # return near_nodes
+        return [node for node in self.node_tree if node.distance(new_node) <= self.search_radius]
 
 
     def check_if_collision_free(self, from_node, to_node):
@@ -135,67 +153,69 @@ class rrt():
         cells = self.bresenham_line(x0, y0, x1, y1)
         #write code here to get values from occupancy grid and check if 1 or 0 exist in the bresenham line cells
         # return a boolean True if no collision
-        for x, y in cells:
-        # Bounds check
-            if x < 0 or x >= len(self.occupancy_grid[0]) or y < 0 or y >= len(self.occupancy_grid):
-                return False
-            if self.occupancy_grid[y][x] == 1: 
-                return False  # Collision detected
+        # for x, y in cells:
+        # # Bounds check
+        #     if x < 0 or x >= len(self.occupancy_grid[0]) or y < 0 or y >= len(self.occupancy_grid):
+        #         return False
+        #     if self.occupancy_grid[y, x] == 1: 
+        #         return False  # Collision detected
 
-        return True  # All clear
+        return all(self.is_valid_node(cell) for cell in cells)  # All clear
+    
 
     def check_if_goal_reached(self):
         if self.debug:
             time.sleep(0.25)
             print("checking if goal reached")
         self.nearest_to_goal_node = self.find_nearest_node(self.goal_node)
-        dx, dy = self.distance(self.nearest_to_goal_node, self.goal_node)
-        dist = math.sqrt(dx**2 + dy**2)
+        # dx, dy = self.distance(self.nearest_to_goal_node, self.goal_node)
+        dist = self.goal_node.distance(self.nearest_to_goal_node) # math.sqrt(dx**2 + dy**2)
         if dist < self.goal_radius:
             self.goal_reached = True
             self.goal_node.parent = self.nearest_to_goal_node
-            self.goal_node.cost = self.nearest_to_goal_node.cost + dist
+            self.goal_node.cost = int(self.nearest_to_goal_node.cost + dist)
 
             self.node_tree.append(self.goal_node)
             if self.debug:
                 time.sleep(1)
-                for i in self.node_tree:
-                    print(i.x, i.y)
+                for node in self.node_tree:
+                    print(node.x, node.y)
                     time.sleep(0.5)
                 
-            self.nearest_to_goal_node = self.goal_node
+            # self.nearest_to_goal_node = self.goal_node
 
-    def rewire(self, new_node, near_nodes):
+
+    def rewire(self, new_node: Node, near_nodes: list[Node]):
         for near_node in near_nodes:
+            
             if near_node == new_node.parent:
                 continue
             
             if self.check_if_collision_free(new_node, near_node):
-                new_cost = new_node.cost + self.distance_to(new_node, near_node)
+                new_cost = new_node.cost + new_node.distance(near_node)
                 if new_cost < near_node.cost:
                     near_node.parent = new_node
                     near_node.cost = new_cost
 
+
     def is_valid_node(self, node):
         x, y = int(node.x), int(node.y)
-        if x < 0 or x >= self.map_size[0] or y < 0 or y >= self.map_size[1]:
-            return False
-        if self.occupancy_grid[y][x] == 1:
-            return False
-        return True
+        # if x < 0 or x >= self.map_size[0] or y < 0 or y >= self.map_size[1]:
+        #     return False
+        # if self.occupancy_grid[y][x] == 1:
+        #     return False
+        # return True
+        return 0 <= x < self.map_size[0] and 0 <= y < self.map_size[1] and self.occupancy_grid[y, x] == 0
+    
 
     def check_if_node_exists(self, node):
-        for n in self.node_tree:
-            if node.x == n.x and node.y == n.y:
-                return False
+        # for n in self.node_tree:
+        #     if node.x == n.x and node.y == n.y:
+        #         return False
         
-        return True
-
-    def distance_to(self, n1, n2):
-        dx = n2.x - n1.x
-        dy = n2.y - n1.y
-        return math.hypot(dx, dy)
-      
+        # return True
+        return node in self.node_tree
+    
 
     def develop_tree(self):
         if self.goal_reached:
@@ -214,11 +234,11 @@ class rrt():
         near_nodes = self.find_near_nodes(new_node)
 
         # Pick best parent
-        min_cost = nearest_node.cost + self.distance_to(nearest_node, new_node)
+        min_cost = nearest_node.cost + nearest_node.distance(new_node)
         best_parent = nearest_node
         for near_node in near_nodes:
             if self.check_if_collision_free(near_node, new_node):
-                cost = near_node.cost + self.distance_to(near_node, new_node)
+                cost = near_node.cost + near_node.distance(new_node)
                 if cost < min_cost:
                     best_parent = near_node
                     min_cost = cost
@@ -237,8 +257,6 @@ class rrt():
         path = []
         current = self.nearest_to_goal_node
 
-        #fill code here
-
         while current is not None:
             path.append((current.x, current.y))
             current = current.parent
@@ -246,7 +264,6 @@ class rrt():
         path.reverse()
         print(f"Path length: {len(path)}")
         return path 
-
 
 
 def main():
@@ -289,7 +306,7 @@ def main():
         [0,0,1,0,1,0,0,0,0,0,1,1,1,0,0,0,0,1,1,1]]
 
 
-    code = rrt(start_pos, goal_pos, map_size, step_size, search_radius, goal_radius, occupancy_grid)
+    code = RRT(start_pos, goal_pos, map_size, step_size, search_radius, goal_radius, occupancy_grid)
     # code.sample_point()
     path = None
 
@@ -351,6 +368,7 @@ def visualize_path(occupancy_grid, path, start, goal):
     plt.yticks(np.arange(0, len(occupancy_grid), 1))
     plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
+
 
 def visualize_tree(node_tree, occupancy_grid, goal_node, start_node, path=None):
     plt.clf()
